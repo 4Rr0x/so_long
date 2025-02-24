2a9659e423b5038fa0c780f735946aa87843679f
4Rr0x <joaopfarreca@gail.com>
2025-02-24 12:47:58 +0000

    gitignore, moves, error handling etc

--- .build/checks.d
+++
@@ -1,8 +0,0 @@
-.build/checks.o: src/checks.c src/../inc/so_long.h \
- src/../inc/../lib/42_libft/libft/libft.h \
- src/../inc/../lib/42_libft/libft/../get_next_line/get_next_line.h \
- src/../inc/../lib/minilibx-linux/mlx.h
-src/../inc/so_long.h:
-src/../inc/../lib/42_libft/libft/libft.h:
-src/../inc/../lib/42_libft/libft/../get_next_line/get_next_line.h:
-src/../inc/../lib/minilibx-linux/mlx.h:
--- .build/handler.d
+++
@@ -1,8 +0,0 @@
-.build/handler.o: src/handler.c src/../inc/so_long.h \
- src/../inc/../lib/42_libft/libft/libft.h \
- src/../inc/../lib/42_libft/libft/../get_next_line/get_next_line.h \
- src/../inc/../lib/minilibx-linux/mlx.h
-src/../inc/so_long.h:
-src/../inc/../lib/42_libft/libft/libft.h:
-src/../inc/../lib/42_libft/libft/../get_next_line/get_next_line.h:
-src/../inc/../lib/minilibx-linux/mlx.h:
